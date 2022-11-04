1b7f74d831c259535dafa28516c2d5bfd2afa201
blawson420 <96959717+blawson420@users.noreply.github.com>
2022-11-04 14:44:28 -0500

    Delete Model directory

--- Model/Color4f.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-struct Color4f
-{
-	float r, g, b, a;
-	Color4f();
-	Color4f(float _a, float _r, float _g, float _b);
-};
-
--- Model/IDeflectionStrategy.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-class IDeflectionStrategy
-{
-public:
-	virtual void Deflect(float &bulletHeading) const = 0;
-	virtual void Destroy() = 0;
-};
